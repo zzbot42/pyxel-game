c843e2a999a3dda7ab871b431aca6b900387506b
zzbot42 <111531628+zzbot42@users.noreply.github.com>
2023-02-10 12:18:25 -0700

    files ya

---
+++ engine.py
@@ -0,0 +1,148 @@
+import pyxel
+from typing import Optional, List, Callable
+from enum import Enum
+
+GRAVITY = 0.75
+TERMINAL_VELOCITY = 20
+PLAYER_SPEED = 6
+JUMP_STRENGTH = 11.5
+
+
+class PhysicsStates(Enum):
+    IDLE = 0
+    FALLING = 1
+    JUMPING = 2
+    RUNNING_LEFT = 3
+    RUNNING_RIGHT = 4
+
+
+class PhysicsStateMachine:
+    def __init__(self):
+        self.state = PhysicsStates.IDLE
+
+
+class Physics:
+    def __init__(self):
+        self.dy = 0
+        self.is_grounded = False
+        self.fall_more = True
+        self.state_machine = PhysicsStateMachine()
+
+    def ground(self):
+        self.dy = 0
+        self.is_grounded = True
+
+    def fall(self, is_colliding=False):
+        if self.is_grounded:
+            self.dy = 0
+            self.fall_more = False
+
+        if self.dy >= GRAVITY:
+            self.is_grounded = False
+            self.state_machine.state = PhysicsStates.FALLING
+        elif self.dy < 0:
+            self.state_machine.state = PhysicsStates.JUMPING
+        else:
+            self.state_machine.state = PhysicsStates.IDLE
+
+        if not is_colliding or not self.is_grounded:
+            self.is_grounded = False
+            self.dy += GRAVITY
+
+            if self.dy > TERMINAL_VELOCITY:
+                self.dy = TERMINAL_VELOCITY
+
+
+class Box:
+    def __init__(
+        self, x: float, y: float, w: float, h: float, col: int, filled=False, phys=False
+    ):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.col = col
+        self.filled = filled
+        self.phys = Physics() if phys else None
+
+    def is_colliding_top(self, box):
+        if (
+            (box.y + box.h >= self.y and box.y <= self.y)
+            and box.phys
+            and (
+                (self.x <= box.x + box.w and self.x + self.w >= box.x + box.w)
+                or (self.x <= box.x and self.x + self.w >= box.x)
+            )
+        ):
+            if box.phys.dy > 0:
+                # knockback
+                box.phys.ground()
+                box.y = self.y - box.h
+                box.phys.dy = 0
+            return True
+        return False
+
+    def fall(self, collider: Callable[[any], bool]):
+        if self.phys:
+            if self.phys.fall_more:
+                # call the collider to see if the object can fall
+                self.phys.fall(collider(self))
+                self.y += self.phys.dy
+                self.phys.fall_more = False
+            # call the collider a second time for knockback
+            collider(self)
+
+    def draw(self):
+        if self.phys:
+            #    print(self.phys.state_machine.state)
+            self.phys.fall_more = True
+        if self.filled:
+            pyxel.rect(self.x, self.y, self.w, self.h, self.col)
+        else:
+            pyxel.rectb(self.x, self.y, self.w, self.h, self.col)
+
+
+class Player(Box):
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        w: float,
+        h: float,
+        col: int,
+        filled=False,
+        keys_move_x_pos: List[int] = [],
+        keys_move_x_neg: List[int] = [],
+        keys_jump: List[int] = [],
+    ):
+        super().__init__(x, y, w, h, col, filled, True)
+        self.keys_move_x_pos = keys_move_x_pos
+        self.keys_move_x_neg = keys_move_x_neg
+        self.keys_jump = keys_jump
+
+    def inputs(self):
+        x = self.x
+        for key in self.keys_move_x_pos:
+            # move x positive
+            if pyxel.btn(key):
+                self.x += PLAYER_SPEED
+                break
+
+        for key in self.keys_move_x_neg:
+            # move x negative
+            if pyxel.btn(key):
+                self.x -= PLAYER_SPEED
+                break
+
+        for key in self.keys_jump:
+            # jump
+            if pyxel.btnp(key) and self.phys and self.phys.is_grounded:
+                self.phys.is_grounded = False
+                self.phys.dy = -JUMP_STRENGTH
+                break
+
+        if self.phys.state_machine.state == PhysicsStates.IDLE:
+            if self.x > x:
+                self.phys.state_machine.state = PhysicsStates.RUNNING_RIGHT
+            elif self.x < x:
+                self.phys.state_machine.state = PhysicsStates.RUNNING_LEFT
---
+++ main.py
@@ -0,0 +1,48 @@
+import pyxel
+from engine import *
+pyxel.image(0).load(0, 0, "filename.pyxres")
+
+SKY_COLOR = 8
+PLAYER_COLOR = 11
+FLOOR_COLOR = 4
+
+
+class App:
+    def __init__(self):
+        pyxel.init(400, 300, title="Platformer", quit_key=pyxel.KEY_Q)
+        self.floors = [
+            Box(0, pyxel.height - 20, pyxel.width, 20, FLOOR_COLOR, filled=True),
+            Box(50, pyxel.height - 100, 40, 20, FLOOR_COLOR, filled=True),
+            Box(50, pyxel.height - 50, 80, 200, FLOOR_COLOR, filled=True),
+
+        ]
+        self.player = Player(
+           20,
+           20,
+           20,
+           20,
+           PLAYER_COLOR,
+           filled=True,
+           keys_move_x_pos=[pyxel.KEY_D, pyxel.KEY_RIGHT],
+           keys_move_x_neg=[pyxel.KEY_A, pyxel.KEY_LEFT],
+           keys_jump=[pyxel.KEY_W, pyxel.KEY_SPACE, pyxel.KEY_UP],
+        )
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+        for floor in self.floors:
+            self.player.fall(floor.is_colliding_top)
+        self.player.inputs()
+        pyxel.camera(self.player.x - 50, 0)
+
+    def draw(self):
+        pyxel.cls(SKY_COLOR)
+
+        for floor in self.floors:
+            floor.draw()
+
+        self.player.draw()
+
+
+App()
